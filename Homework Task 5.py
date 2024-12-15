5ea7cdefe56607ddbd921dbfd74df0669cf4bbb5
Avanthikacoding <aviedayilliam@gmail.com>
2024-12-15 11:18:11 +0000

    Add files via upload

---
+++ Homework Task 5.py
@@ -0,0 +1,13 @@
+summer_clothes = input("Enter what clothes you are wearing:")
+if summer_clothes == "a shirt and shorts":
+    print("You are wearing the right clothes! YOur good to go! Bye! Have fun at the park 😜")
+elif summer_clothes == "a full sleeve shirt and a skirt":
+    print("I think you will be a bit sweaty with a sleev shirt on but your good to go! Have fun at the park 😜")
+elif summer_clothes == "something comfy and a light jacket":  
+    print("A light jacket is good if it rains but make sure you still wear your sunscream! Bye! Have fun at the park 😜")
+elif summer_clothes == "just a casual outfit":
+    print("Thst is good! The prefect outwear for the summer! Your good to go! Bye! Have fun at the park 😜")
+elif summer_clothes == "a normal summer outwear":
+    print("Just perfect! You're good to go! Bye! Have fun at the park 😜")
+else: 
+    print("You have the wrong clothes for this weather! Go change right now 😡 and only come back when you are weraing the right clothes for summer🙅‍♀️! I ddin't expect a mature enough child to be wearing that for summer 🤦‍♀️")
